35a1a3f66ff8863fce6d97904aa6a5de8be15984
Lakshantha <lakshanthad@gmail.com>
2024-05-29 19:08:36 -0700

    Added updates

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+// load environment variables
+// check to see if are running in production environment or development environment 
+// dotenv library only used for development. if production, we need to remove dotenv library
+// NODE_ENV variable that is set by Node to tell you what environment you are on
+if (process.env.NODE_ENV !== 'production'){
+    // the below will load all the variables in .env and put them inside process.env variable in the server
+    require('dotenv').config()
+}
+
+// reference environment variables inside the server
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+console.log(stripeSecretKey, stripePublicKey)
+
 const express = require('express')
 const app = express()
 
